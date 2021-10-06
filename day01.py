2b5a0626917c20eee3bc045e546c3425086f67d1
俊 巡 <65719973+jun-xun@users.noreply.github.com>
2021-10-06 18:24:30 +0800

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,142 @@
+# print("Hello python")
+
+# a = input("请输入年龄：") # a = 我们输入的内容
+# print(a)
+
+# b = input("发车了：")
+# print(b)
+
+# name = 99 # 
+
+"""""  数据类型  """
+# int
+112
+# int 
+999
+# int 
+777
+# int
+33
+# int
+88
+
+# 字符串
+"小团团"
+# 字符串
+'法外狂徒'
+# 字符串
+'张三'
+# 字符串
+"国庆快乐"
+# 字符串
+"假期不足"
+ 
+# 布尔类型 bool
+True  # 真
+False # 假
+# 布尔类型 bool
+True # 真
+False # 假
+# 布尔类型 bool
+True # 真
+False # 假
+# 布尔类型 bool
+False # 假
+True # 真
+# 布尔类型 bool
+True # 真
+False # 假
+
+# 空 NoneType
+None
+# 空 NoneType
+None 
+# 空 NoneType
+None
+# 空 NoneType
+None
+# 空 NoneType
+None
+
+# list 数组
+[1,2,3]
+# list 数组
+[1,2,3]
+# list 数组
+[1,2,3,4]
+# list 数组
+[3,5,2]
+# list 数组
+[3,3,4]
+
+# tuple 元组
+a = (1,2,"3")
+# a.append(4)  # 元组不能用append, 元组不能修改
+# print(a)
+# tuple 元组
+(1,2,"3")
+# tuple 元组
+(1,2,"3")
+# tuple 元组
+(1,2,"3")
+# tuple 元组
+(1,2,"3")
+
+# dict 字典
+{"xm":"liao","yz":"高"}
+# dict 字典
+{"xm":";liao","sg":"高"}
+# dict 字典
+{"name":"junxiong","gender":"male"}
+# dict 字典
+{"name":"liao","sg":"high","zs":"high"}
+# high 字典
+{"name":"lili","gender":"female"}
+
+a = 1.2 + 1.6
+print(type(a))
+
+r1 = "x加{}".format("y")
+print(r1)
+
+# # 字符串查找
+a = "小团团"
+# r2 = a.find("大") # 找到了就返回查找内容在字符串中的位置
+# print(r2)
+
+# 统计字符出现的次数
+# r3 = a.count("团")
+# print(r3)
+
+# 字符串的替换
+# cc = a.replace("小","大")
+# print(cc)
+
+# 获取字符串的下标
+# print(a.index("小"))
+# print(len(a))  
+
+a = [1,2,3,"小团团",1]
+# print(a[0])
+# print(a[1])
+# print(a[-2])
+
+# 增加
+# a.append("小摩托")
+# print(a)
+
+# 修改
+# a[-1] = "大团团"
+# print(a)
+
+# 删除
+# a.remove("小团团")
+# print(a)
+
+# 统计个数
+# print(a.count(1))
+
+# 数组排序
+b = [1,23,20,4,3] # -->[1,3,4,20,23]
+b.sort(reverse = True)
+print(b)
